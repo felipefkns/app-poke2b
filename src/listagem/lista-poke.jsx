b17ce375b9b383801b70c61545de20e59bf20c28
felipefkns <felipenascsilva8@gmail.com>
2024-05-07 09:14:21 -0300

    pokemon

---
+++ src/listagem/lista-poke.jsx
@@ -0,0 +1,16 @@
+const Listagem = ()=> 
+ 
+    (
+        <>
+            <div>
+                <ul>
+                    <li>Pokemon 1</li>
+                    <li>Pokemon 2</li>
+                    <li>Pokemon 3</li>
+                </ul>
+            </div>
+        </>
+    )
+
+ 
+export default Listagem
